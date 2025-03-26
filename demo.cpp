16d51b6af4fc7918d7ecb656c7ac0b91a3f539bf
dhavalrw6 <rw5.dhaval.pl@gmail.com>
2025-03-26 09:49:00 +0530

    update1

--- demo.cpp
+++ demo.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main(){
 
-    cout<<"Hello World!";
+    int a,b,sum;
+
+    cout<<"Enter 'a' number: ";
+    cin>>a;
+    cout<<"Enter 'b' number: ";
+    cin>>b;
+
+    sum = a+b;
+    cout<<"sum of a and b is: "<<sum;
 
     return 0;
 }
